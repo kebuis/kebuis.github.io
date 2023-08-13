f936a19972077f1e9a8a4ffde6db4ee1d97f5d4a
kebuis <1192507944@qq.com>
2023-08-13 13:34:38 +0800

    Site updated: 2023-08-13 13:34:34

--- js/lib/math.js
+++ js/lib/math.js
@@ -1,1 +1,1 @@
-mixins.math={data(){return{renderers:[this.math]}},methods:{math(){renderMathInElement(document.body,{delimiters:[{left:"$$",right:"$$",display:!0},{left:"$",right:"$",display:!1},{left:"\\(",right:"\\)",display:!1},{left:"\\[",right:"\\]",display:!0}]})}}};
+mixins.math={created(){this.renderers.push(this.math)},methods:{math(){renderMathInElement(document.body,{delimiters:[{left:"$$",right:"$$",display:!0},{left:"$",right:"$",display:!1},{left:"\\(",right:"\\)",display:!1},{left:"\\[",right:"\\]",display:!0}]})}}};
